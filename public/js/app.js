53e2d4e73dc3d6258b80d3dc27ed6df5958c73ab
Zoli06 <63035911+Zoli06@users.noreply.github.com>
2020-11-21 11:55:32 +0100

    Messages works

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,91 @@
+'use strict';
+
+var messageBox;
+var chatMessages;
+$(document).ready(function() {
+  messageBox = document.getElementById('message');
+  chatMessages = document.getElementById('chat-messages');
+});
+
 const socket = io();
+
+socket.on('inputMessage', message => {
+  console.log(message);
+  inputMessage(message);
+  scrollAndClearMsg();
+});
+
+socket.on('yourMessage', message => {
+  console.log(message);
+  outputMessage(message);
+  scrollAndClearMsg();
+});
+
+socket.on('globalMessage', message => {
+  console.log(message);
+  globalMessage(message);
+  scrollAndClearMsg();
+});
+
+function submitMessage(caller) {
+  const message = messageBox.value;
+
+  socket.emit('chatMessage', message);
+
+  return false;
+}
+
+function outputMessage(message) {
+  var div = document.createElement('div');
+  div.classList.add('outgoing-message', 'conversation');
+  var currentTime = Date.now();
+  div.innerHTML = '<p class="meta">Me <span>' + getTime(true) + '</span></p><p class="message-text">' + message + '</p>';
+  chatMessages.appendChild(div);
+}
+
+function inputMessage(message) {
+  var div = document.createElement('div');
+  div.classList.add('incoming-message', 'conversation');
+  var currentTime = Date.now();
+  div.innerHTML = '<p class="meta">Me <span>' + getTime(true) + '</span></p><p class="message-text">' + message + '</p>';
+  chatMessages.appendChild(div);
+}
+
+function globalMessage(message) {
+  var div = document.createElement('div');
+  div.classList.add('global-message');
+  var currentTime = Date.now();
+  div.innerHTML = '<p>' + message + '</p>';
+  chatMessages.appendChild(div);
+}
+
+function getTime(format) {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  var minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if(format == true) { //12h
+    var part = 'am';
+    if(hours > 12) {
+      hours -= 12;
+      part = 'pm'
+    }
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+    return hours + ':' + minutes + part;
+  } else {
+    return hours + ':' + minutes;
+  }
+}
+
+function scrollAndClearMsg() {
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+  messageBox.value = '';
+  messageBox.focus();
+}
--- server.js
+++ server.js
@@ -9,6 +9,23 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+io.on('connection', socket => {
+  console.log('New connection...');
+
+  socket.emit('globalMessage', 'You connected')
+  socket.broadcast.emit('globalMessage', 'A user has joined');
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message', 'A user has disconnected!');
+    console.log('Closed a connection...')
+  });
+
+  socket.on('chatMessage', message => {
+    socket.emit('yourMessage', message);
+    socket.broadcast.emit('inputMessage', message);
+  });
+});
+
 const PORT = 4000 || process.env.PORT;
 
 server.listen(PORT, () => console.log('Server running on port ' + PORT));
